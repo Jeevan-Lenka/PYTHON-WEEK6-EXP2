1f27260319167a5e7a342b96dbe742c0f6692cd9
Jeevan-Lenka <24331a05g0@mvgrce.edu.in>
2026-02-20 14:43:04 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,9 @@
+name=input("Enter any string : ")
+print("Your string : ",name)
+reverse=name[::-1]
+print("Your Reverse string : ",reverse)
+if(name==reverse):
+    print("Given string is palindrome")
+else :
+    print("Given string is not palindrome")
+
